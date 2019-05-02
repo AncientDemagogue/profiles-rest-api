031dd8163d234b24db5328773defe3f843edf68f
AncientDemagogue <domagojbakota@gmail.com>
2019-05-02 14:39:51 +0200

    Added improved permisions to user profile api.

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -10,5 +10,17 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.id == request.user.id 
+        return obj.id == request.user.id
         # returns true if the user is changing object with its own id(his own profile)
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status."""
+
+    def has_object_permission(self, request, view, obj):
+        """checks the user is trying to update their own status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
+               
